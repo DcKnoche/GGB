44f77c9d47560b72a355774030390f50974bc73f
luisgumer95 <allworldinhere@gmail.com>
2015-03-09 22:43:12 -0430

    Adding Cout and Cin

--- Bases Convertidas.cpp
+++ Bases Convertidas.cpp
@@ -2,25 +2,27 @@
 #include <stdlib.h>
 #include<iostream>
    int numerosiguiente, base, indice=0;
-   int numeroconvertido[64];
-   int numeroaconvertir;
+   int numerotransformado[64];
+   int numeroatransformar;
    char digitobase[17] =
      {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G'};
 main(void){
 	using namespace std;
-    printf("Mete el numero y la base deseada: ");
-    scanf("%d %i", &numeroaconvertir, &base);
-    while (numeroaconvertir != 0)
+    cout<<"Introdusca numero a transforma: ";
+    cin>> numeroatransformar;
+    cout<<"Introdusca Base:";
+    cin>> base; 
+    while (numeroatransformar != 0)
    {
-     numeroconvertido[indice] = numeroaconvertir % base;
-     numeroaconvertir = numeroaconvertir / base;
+     numerotransformado[indice] = numeroatransformar % base;
+     numeroatransformar = numeroatransformar / base;
      ++indice;
    }
    --indice;
-   printf("\n\n Numero convertido = ");
+   cout<<"\n\n Numero transformado = ";
    for(  ; indice>=0; indice--) 
    {
-   printf("%c", digitobase[numeroconvertido[indice]]);
+   cout<<("%c", digitobase[numerotransformado[indice]]);
 	 }
-   printf("\n");
+   cout<<"\n";
 }
