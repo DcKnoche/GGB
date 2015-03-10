69bd9f89850f53f9dad4f1704ab582236f211e27
Heradocles <heradocles@gmail.com>
2015-03-09 22:25:28 -0430

    Nuevo Nuevo

--- Bases Convertidas.cpp
+++ Bases Convertidas.cpp
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-main(void)
-{
-   char digitobase[17] =
-     {'0', '1', '2', '3', '4', '5', '6', '7',
-      '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+#include<iostream>
+   int numerosiguiente, base, indice=0;
    int numeroconvertido[64];
    int numeroaconvertir;
-   int numerosiguiente, base, indice=0;
-   printf("Mete el numero y la base deseada: ");
-   scanf("%d %i", &numeroaconvertir, &base);
-   while (numeroaconvertir != 0)
+   char digitobase[17] =
+     {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+main(void){
+	using namespace std;
+    printf("Mete el numero y la base deseada: ");
+    scanf("%d %i", &numeroaconvertir, &base);
+    while (numeroaconvertir != 0)
    {
      numeroconvertido[indice] = numeroaconvertir % base;
      numeroaconvertir = numeroaconvertir / base;
@@ -18,9 +18,9 @@ main(void)
    }
    --indice;
    printf("\n\n Numero convertido = ");
-   for(  ; indice>=0; indice--)
+   for(  ; indice>=0; indice--) 
    {
-     printf("%c", digitobase[numeroconvertido[indice]]);
-   }
+   printf("%c", digitobase[numeroconvertido[indice]]);
+	 }
    printf("\n");
 }
