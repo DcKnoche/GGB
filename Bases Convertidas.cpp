25d2a24ff4d39a080b0e37d8ae4eb5a27f8d1002
Heradocles <heradocles@gmail.com>
2015-03-09 22:45:10 -0430

    Fix 1

--- Bases Convertidas.cpp
+++ Bases Convertidas.cpp
@@ -8,7 +8,7 @@
      {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G'};
 main(void){
 	using namespace std;
-    cout<<"Introdusca numero a transforma: ";
+    cout<<"Introdusca numero a transformar: ";
     cin>> numeroatransformar;
     cout<<"Introdusca Base:";
     cin>> base; 
