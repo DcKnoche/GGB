a67806a3c0a6f3aff549e681b7818749b7bc54ab
Heradocles <heradocles@gmail.com>
2015-03-09 22:06:15 -0430

    Correccion

--- Bases Convertidas.cpp
+++ Bases Convertidas.cpp
@@ -1,33 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-void main(void)
+main(void)
 {
-   char base_digits[17] =
+   char digitobase[17] =
      {'0', '1', '2', '3', '4', '5', '6', '7',
       '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G'};
-
    int numeroconvertido[64];
    long int numeroaconvertir;
    int numerosiguiente, base, indice=0;
-
-   /* Agarra el numero y la base */
    printf("Mete el numero y la base deseada: ");
    scanf("%ld %i", &numeroaconvertir, &base);
-
-   /* Convertimos el numero */
    while (numeroaconvertir != 0)
    {
-     numeroconvertido[index] = numeroaconvertir % base;
+     numeroconvertido[indice] = numeroaconvertir % base;
      numeroaconvertir = numeroaconvertir / base;
      ++indice;
    }
-
-   /* Imprimimos el resultado */
    --indice;
    printf("\n\n Numero convertido = ");
-   for(  ; indice>=0; indice--) /* Echamos para atras el arreglito */
+   for(  ; indice>=0; indice--)
    {
-     printf("%c", base_digits[numeroconvertido[indice]]);
+     printf("%c", digitobase[numeroconvertido[indice]]);
    }
    printf("\n");
 }
