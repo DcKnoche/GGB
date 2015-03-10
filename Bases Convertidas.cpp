e47eaddfb49644fbac04423c24522cc096f8b9b0
Heradocles <heradocles@gmail.com>
2015-03-10 00:20:04 -0430

    Usemos un Swicht

--- Bases Convertidas.cpp
+++ Bases Convertidas.cpp
@@ -1,28 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 #include<iostream>
-   int base, indice=0;
-   int numtrans[64];
-   int numatrans;
+   int op, base, indice=0;
+   int numerotransformado[64];
+   int numeroatransformar;
    char digitobase[17] =
      {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G'};
 main(void){
 	using namespace std;
+	case1:
+	system("cls");
+cout<<"Bienvenido a CambiaTuBaseConEstilo3000 \n";
+cout<<"Eliga una opcion: \n";
+cout<<"1. Introducir un numero \n";
+cout<<"2. Salir :( \n";
+cin>>op;
+switch (op)
+{
+	case 1: 
+	system("cls");
     cout<<"Introdusca numero a transformar: ";
-    cin>> numatrans;
-    cout<<"Introdusca Base:";
+    cin>> numeroatransformar;
+    cout<<"Introdusca Base(Por falta de presupuesto, solo de base 2 a 17, gracias, la gerencia):";
     cin>> base; 
-    while (numatrans != 0)
+    while (numeroatransformar != 0)
    {
-     numtrans[indice] = numatrans % base;
-     numatrans = numatrans/ base;
+     numerotransformado[indice] = numeroatransformar % base;
+     numeroatransformar = numeroatransformar / base;
      ++indice;
    }
    --indice;
    cout<<"\n\n Numero transformado = ";
    for(  ; indice>=0; indice--) 
    {
-   cout<<("%c", digitobase[numtrans[indice]]);
+   cout<<("%c", digitobase[numerotransformado[indice]]);
 	 }
    cout<<"\n";
+   cout<<"Volver al menu principal? (1. Si 2. No)";
+   cin>>op;
+   if(op==1){
+   goto case1;
+   }
+   else{
+   goto case2;
+   }
+   break;
+   case 2: 
+   case2:
+   exit ( 0 );
+   break;
+}
 }
